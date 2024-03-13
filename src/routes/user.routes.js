5760d108bf2c4d62b6cb5b1bf6ebe766e36c37ac
Sanskar-13 <sanskar06march@gmail.com>
2024-03-13 08:43:28 +0530

    wrote logic for user registration

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js"; //import is done this way when export is like export {registerUser}
-
+import {upload} from "../middlewares/multer.middlewar.js"
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar", //this name must be same in frontend as well
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
 
 export default router;
 
